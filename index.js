4c596ac796e881a93010e9e1d3e3dd8ad784753f
Matheus <matheusb86@gmail.com>
2022-05-27 23:33:28 -0300

    atualziação de codigo

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let c = [0,0,0,0,1,0]
-
 let countjump = 0
 
 function jumping(){
+    
     for ( i = 0; i < c.length; i++){
-        if(c[i] === 0){
-                countjump++
-                i++      
-                   
-        }else {
+            if(c[i+2] === 0 && i + 2 < c.length ){
             countjump++
-            i++
-        }    
+            i = i + 2
+            
+        }if(c[i+1] === 0 && i + 1 < c.length){
+            countjump++   
+        }
     }
     return countjump
 }
-console.log(jumping(c))
+var result = jumping(c)
+
+console.log(result)
